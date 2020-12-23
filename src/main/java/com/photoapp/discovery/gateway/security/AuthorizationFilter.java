e50cfb466f610308ae41fac4267ebd7494b34b9e
Oleg Bezkorovaynyi <oleg.bezkorovaynyi@globallogic.com>
2020-12-24 01:52:42 +0200

    enable spring security on zuul gateway

---
+++ src/main/java/com/photoapp/discovery/gateway/security/AuthorizationFilter.java
@@ -0,0 +1,71 @@
+package com.photoapp.discovery.gateway.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.core.env.Environment;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Collections;
+
+public class AuthorizationFilter extends BasicAuthenticationFilter {
+
+    private final Environment environment;
+
+    public AuthorizationFilter(
+            AuthenticationManager authenticationManager,
+            Environment environment
+    ) {
+        super(authenticationManager);
+        this.environment = environment;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain chain
+    ) throws IOException, ServletException {
+        String authorizationHeader = request.getHeader(environment.getProperty("authorization.token.header.name"));
+
+        if (authorizationHeader == null || !authorizationHeader.startsWith(environment.getProperty("authorization.token.header.prefix"))) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken authentication = getAuthentication(request);
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+    }
+
+    private UsernamePasswordAuthenticationToken getAuthentication(HttpServletRequest req) {
+        String authorizationHeader = req.getHeader(environment.getProperty("authorization.token.header.name"));
+
+        if (authorizationHeader == null) {
+            return null;
+        }
+
+        String token = authorizationHeader.replace(environment.getProperty("authorization.token.header.prefix"), "");
+
+        String userId = Jwts.parser()
+                .setSigningKey(environment.getProperty("token.secret"))
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+
+        // perform validations against values, stored in JWT token
+
+        if (userId == null) {
+            return null;
+        }
+
+        return new UsernamePasswordAuthenticationToken(userId, null, Collections.emptyList());
+    }
+}
